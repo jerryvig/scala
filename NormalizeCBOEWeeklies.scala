ff3efea34f64d142722060a3832fbdbe5aec3a0d
Jerry Vigil <jerry.vigil@mktneutral.com>
2012-02-24 14:40:38 -0800

    Next commit for changes to sharpe_ratio calculation and ranking algo.

--- NormalizeCBOEWeeklies.scala
+++ NormalizeCBOEWeeklies.scala
@@ -20,7 +20,7 @@ object NormalizeCBOEWeeklies {
     
     stmt.executeUpdate("CREATE OR REPLACE TRIGGER cboe_wo_trigger BEFORE INSERT ON cboe_weeklies_ordered REFERENCING NEW AS NEW FOR EACH ROW BEGIN SELECT cboe_weekly_returns_seq.nextval INTO :NEW.IDX FROM dual; END;")
 
-    stmt.executeUpdate("INSERT INTO cboe_weeklies_ordered ( eod_date, ticker_symbol, close, adj_close ) SELECT eod_date, ticker_symbol, close, adj_close FROM cboe_weeklies_eod ORDER BY ticker_symbol ASC, eod_date ASC")
+    stmt.executeUpdate("INSERT INTO cboe_weeklies_ordered ( eod_date, ticker_symbol, close, adj_close ) SELECT eod_date, ticker_symbol, close, adj_close FROM cboe_weeklies_eod WHERE eod_date>'01-JAN-11' ORDER BY ticker_symbol ASC, eod_date ASC")
 
      try {
       stmt.executeUpdate("DROP TABLE cboe_weeklies_returns")
@@ -39,7 +39,7 @@ object NormalizeCBOEWeeklies {
 
    stmt.executeUpdate("CREATE TABLE cboe_weeklies_sharpe ( ticker_symbol VARCHAR(10), avg_return DOUBLE PRECISION, sharpe_ratio DOUBLE PRECISION )")
 
-   stmt.executeUpdate("INSERT INTO cboe_weeklies_sharpe SELECT ticker_symbol, AVG(day_change), AVG(day_change)/STDDEV(day_change) FROM cboe_weeklies_returns WHERE eod_date>'01-JAN-11' GROUP BY ticker_symbol")
+   stmt.executeUpdate("INSERT INTO cboe_weeklies_sharpe SELECT ticker_symbol, AVG(day_change), AVG(day_change)/STDDEV_POP(day_change) FROM cboe_weeklies_returns GROUP BY ticker_symbol")
 
     //For revenue growth data from Morningstar
     try {
@@ -72,7 +72,59 @@ object NormalizeCBOEWeeklies {
     
     stmt.executeUpdate("CREATE TABLE cboe_sharpe_revg ( ticker_symbol VARCHAR(10), avg_rev_growth DOUBLE PRECISION, sharpe_rev_growth DOUBLE PRECISION )")
 
-    stmt.executeUpdate("INSERT INTO cboe_sharpe_revg SELECT ticker_symbol, AVG(rev_growth), AVG(rev_growth)/STDDEV(rev_growth) FROM cboe_revenue_growth GROUP BY ticker_symbol")
+    stmt.executeUpdate("INSERT INTO cboe_sharpe_revg SELECT ticker_symbol, AVG(rev_growth), AVG(rev_growth)/STDDEV_POP(rev_growth) FROM cboe_revenue_growth GROUP BY ticker_symbol")
+
+    try {
+      stmt.executeUpdate("DROP SEQUENCE rank_seq");
+    } catch { case e:Exception => }
+
+    stmt.executeUpdate("CREATE SEQUENCE rank_seq START WITH 1 INCREMENT BY 1");
+   
+    try {
+     stmt.executeUpdate("DROP TABLE sharpe_ratio_ranks");
+    } catch { case e:Exception => }
+
+    stmt.executeUpdate("CREATE TABLE sharpe_ratio_ranks ( idx INTEGER, ticker_symbol VARCHAR(10) )")
+
+    try {
+      stmt.executeUpdate("DROP TRIGGER sharpe_ranks_trigger")
+    } catch { case e:Exception => }
+
+    stmt.executeUpdate("CREATE OR REPLACE TRIGGER sharpe_ranks_trigger BEFORE INSERT ON sharpe_ratio_ranks REFERENCING NEW AS NEW FOR EACH ROW BEGIN SELECT rank_seq.nextval INTO :NEW.IDX FROM dual; END;")
+
+    stmt.executeUpdate("INSERT INTO sharpe_ratio_ranks (ticker_symbol) SELECT ticker_symbol FROM cboe_weeklies_sharpe ORDER BY sharpe_ratio DESC");
+
+    try { 
+     stmt.executeUpdate("DROP SEQUENCE rank_seq")
+    } catch { case e:Exception => }
+
+    stmt.executeUpdate("CREATE SEQUENCE rank_seq START WITH 1 INCREMENT BY 1");
+
+    try {
+     stmt.executeUpdate("DROP TABLE sharpe_revg_ranks")
+     } catch { case e:Exception => }
+
+    stmt.executeUpdate("CREATE TABLE sharpe_revg_ranks ( idx INTEGER, ticker_symbol VARCHAR(10) )")
+
+    try {
+      stmt.executeUpdate("DROP TRIGGER sharpe_ranks_trigger")
+    } catch { case e:Exception => }
+
+    stmt.executeUpdate("CREATE OR REPLACE TRIGGER sharpe_ranks_trigger BEFORE INSERT ON sharpe_revg_ranks REFERENCING NEW AS NEW FOR EACH ROW BEGIN SELECT rank_seq.nextval INTO :NEW.IDX FROM dual; END;");
+
+    stmt.executeUpdate("INSERT INTO sharpe_revg_ranks (ticker_symbol) SELECT ticker_symbol FROM cboe_sharpe_revg ORDER BY sharpe_rev_growth DESC")
+
+    try {
+      stmt.executeUpdate("DROP TABLE cboe_norm_ranks")
+    } catch { case e:Exception => }
+
+    stmt.executeUpdate("CREATE TABLE cboe_norm_ranks ( ticker_symbol VARCHAR(10), norm_rank DOUBLE PRECISION )");
+
+    stmt.executeUpdate("INSERT INTO cboe_norm_ranks SELECT t1.ticker_symbol, SQRT(t1.idx*t1.idx+t2.idx*t2.idx) AS rank_norm FROM sharpe_ratio_ranks t1, sharpe_revg_ranks t2 WHERE (t2.ticker_symbol=t1.ticker_symbol) order by rank_norm ASC")
+
+    try {
+     stmt.executeUpdate("DROP SEQUENCE rank_seq")
+    } catch { case e:Exception => }
 
     conn.close()    
   } 
