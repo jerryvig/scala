15780926328a5d84582586accf10aa2208ce9efb
Jerry Vigil <jerry.vigil@mktneutral.com>
2012-03-03 19:49:16 -0800

    7:49 PM commit

--- HitLinkedIn.bash
+++ HitLinkedIn.bash
@@ -4,4 +4,7 @@ source /home/diggler/.bash_profile
 /usr/bin/links http://www.linkedin.com &
 sleep 8;
 /usr/bin/killall links
+/usr/bin/links http://www.cybercoders.com &
+sleep 9;
+/usr/bin/killall links
 exit 0
